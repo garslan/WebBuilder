98812ffdd82cfd18af059120b6f5190844a782b8
CoolerB <bodiamail@gmail.com>
2015-01-22 18:34:15 +0200

    Update Generate.java

--- src/main/java/com/app/logic/Generate.java
+++ src/main/java/com/app/logic/Generate.java
@@ -10,21 +10,21 @@ public class Generate {
     public static void main(String[] args) {
 
         //Generate widgets
-//        WidgetDAOImpl widgetDAO = new WidgetDAOImpl();
-//
-//        for (int i = 0; i < 10; i++) {
-//            Widget widget = new Widget();
-//            widget.setId(String.valueOf(UUID.randomUUID()));
-//            widget.setName("Widget-" + i);
-//            widget.setImageUrl("images/widgets/" + i);
-//
-//            Map<String, String> settings = new HashMap<>();
-//            for (int j = 0; j < 5; j++) {
-//                settings.put("text" + j, "textvalue" + j);
-//            }
-//            widget.setSettings(settings);
-//            widgetDAO.saveWidget(widget);
-//        }
+        WidgetDAOImpl widgetDAO = new WidgetDAOImpl();
+
+        for (int i = 0; i < 10; i++) {
+            Widget widget = new Widget();
+            widget.setId(String.valueOf(UUID.randomUUID()));
+            widget.setName("Widget-" + i);
+            widget.setImageUrl("images/widgets/" + i);
+
+            Map<String, String> settings = new HashMap<>();
+            for (int j = 0; j < 5; j++) {
+                settings.put("text" + j, "textvalue" + j);
+            }
+            widget.setSettings(settings);
+            widgetDAO.saveWidget(widget);
+        }
 
 
         //Generate projects
